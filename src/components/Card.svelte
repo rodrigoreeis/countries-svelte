44ed3817274324a51ea38de543091af2170b15ca
Rodrigo Reis <br.rodrigoreis@gmail.com>
2020-05-30 00:09:01 -0300

    :sparkles: create card countrie and show countries home

---
+++ src/components/Card.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let flag
+  export let name
+  export let population
+  export let region
+  export let capital
+</script>
+
+<template>
+  <div class='card-countrie'>
+    <figure class='card-countrie__flag'>
+      <img src={flag} alt={name} />
+    </figure>
+    <div class='card-countrie__content'>
+      <h1 class='card-countrie__name'>{name}</h1>
+      <h1 class='card-countrie__population'>Population: {population}</h1>
+      <h1 class='card-countrie__region'>Region: {region}</h1>
+      <h1 class='card-countrie__capital'>Capital: {capital}</h1>
+    </div>
+  </div>
+</template>
+
+<style lang='scss'>
+
+</style>
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,13 +1,32 @@
 <script>
   import api from '../services/api'
-  
+  import Card from '../components/Card.svelte'
+
   let promise = api('/all')
+  
 </script>
 
 <template>
-  <div class='wrapper'>
-    <p class='wrapper__text'>dwokdwokwo</p>
-  </div>
+  <section class='home'>
+    {#await promise}
+      <p>...loading</p>
+    {:then countries}
+      {#each countries as countrie}
+        <Card 
+          flag={countrie.flag}
+          name={countrie.name} 
+          population={countrie.population}
+          region={countrie.region}
+          capital={countrie.capital}
+        />
+      {/each}
+    {:catch error}
+      <p>{error.message}</p>
+    {/await}
+
+
+
+  </section>
 </template>
 
 <style lang="scss">
