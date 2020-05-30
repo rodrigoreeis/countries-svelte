26bd997f5035a0355d9382ccf181a9a9de29c4f5
Rodrigo Reis <br.rodrigoreis@gmail.com>
2020-05-30 11:50:02 -0300

    :lipstick: finish style to search input

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -20,19 +20,43 @@
   />
 </div>
 
-<style lang='scss'>
+<style lang='scss' global>
+  @import '../styles/tools/index.scss';
 
   .search {
     
     width: 100%;
-    border: 1px solid slategray;
     margin-bottom: 20px;
     margin-top: 20px;
+    @include box-shadow;
+    border-radius: 5px;
+    display: flex;
+
+    @include themify($themes) {
+
+      background-color: themed('backgroundElements');
+      color: themed('textColor')
+
+    }
 
     &__input {
 
       width: 100%;
       padding: 15px;
+      font-weight: 500;
+      font-size: 16px;
+      
+      &::placeholder {
+
+        font-weight: 500;
+        font-size: 14px;
+
+      }
+      @include themify($themes) {
+
+        color: themed('textColor')
+
+      }
 
     }
 
