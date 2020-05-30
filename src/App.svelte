ee0aff1fbac3fcbf9f9afbcb841dc28e06b9ee26
Rodrigo Reis <br.rodrigoreis@gmail.com>
2020-05-29 22:12:00 -0300

    :tada: create app

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { Router, Link, Route } from 'svelte-routing';
+  import Home from './routes/Home.svelte';
+  import Countrie from './routes/Countrie.svelte';
+
+  export let url = '';
+</script>
+
+<Router url='{url}'>
+  <nav>
+    <Link to='/'>Home</Link>
+    <Link to='/countrie'>countrie</Link>
+  </nav>
+  <div>
+    <Route path='/countrie'>
+			<Countrie />
+		</Route>
+    <Route path='/'><Home /></Route>
+  </div>
+</Router>
+
+<style lang='scss' global>
+  @import './styles/app.scss';
+</style>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,12 @@
+<style lang="scss">
+  .wrapper {
+    background-color: blue;
+    &__text {
+      color: red
+    }
+  }
+</style>
+
+<div class='wrapper'>
+  <p class='wrapper__text'>dwokdwokwo</p>
+</div>
